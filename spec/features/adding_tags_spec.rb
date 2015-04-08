e94d21df3ca0435d96b0497876ca98f1cae84374
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-08 11:24:58 +0100

    Adding tag support in pair

---
+++ spec/features/adding_tags_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'adding tags to links' do
+  scenario 'with a few tags' do
+    visit '/'
+    add_link('http://www.makersacademy.com',
+             'Makers Academy',
+             %w(education ruby))
+    link = Link.first
+    expect(link.tags.map(&:text)).to include('education', 'ruby')
+  end
+
+  def add_link(url, title, tags = [])
+    within('#new-link') do
+      fill_in 'url', with: url
+      fill_in 'title', with: title
+      fill_in 'tags', with: tags.join(' ')
+      click_button('Add link')
+    end
+  end
+end
