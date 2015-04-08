621b3b29fc5ef65758089e7365c6fe2a9ce83c6c
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-08 12:44:44 +0100

    Added ability to retrieve links filtered by tag

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -3,11 +3,29 @@ require 'spec_helper'
 feature 'User browses the list if links' do
   before(:each) do
     Link.create(title: 'Makers Academy',
-                url:   'http://www.makeracademy.com/')
+                url:   'http://www.makeracademy.com/',
+                tags:  [Tag.first_or_create(text: 'education')])
+    Link.create(title: 'Google',
+                url:   'http://www.google.com/',
+                tags:  [Tag.first_or_create(text: 'search')])
+    Link.create(title: 'Bing',
+                url:   'http://www.bing.com/',
+                tags:  [Tag.first_or_create(text: 'search')])
+    Link.create(title: 'Code.org',
+                url:   'http://www.code.org/',
+                tags:  [Tag.first_or_create(text: 'education')])
   end
 
   scenario 'when opening the home page' do
     visit('/')
     expect(page).to have_content('Makers Academy')
+  end
+
+  scenario 'filtered by a tag' do
+    visit '/tags/search'
+    expect(page).not_to have_content('Makers Academy')
+    expect(page).not_to have_content('Code.org')
+    expect(page).to have_content('Google')
+    expect(page).to have_content('Bing')
   end
 end
