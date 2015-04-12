aa3b96bf9ecd4cb97b8d594bd59ac17786a04f43
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-12 14:27:41 +0100

    Added database consistency tasks to Rakefile

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,5 +1,5 @@
 env = ENV['RACK_ENV'] || 'development'
 
+# DataMapper::Logger.new(STDOUT, :debug)
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
